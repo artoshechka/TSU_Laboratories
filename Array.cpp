e642c846725ba019adfa84f079d2976e00937980
Anton Artemenko <82308201+artoshechka@users.noreply.github.com>
2023-12-19 01:58:41 +0700

    жопа

--- Array.cpp
+++ Array.cpp
@@ -1,226 +1,318 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <ctime>
-#include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
-class Array {
+class Array
+{
     int *a, n;
 
 public:
-    // Конструктор 1
+    // конструктор 1
+    // len – число элементов в массиве
+    // t = 1 – неупорядоченный массив
+    // t = 2 – массив, упорядоченный по неубыванию
+    // t = 3 – массив, упорядоченный по невозрастанию
+    // d – диапазон псевдослучайных чисел для неупорядоченного массива (при t = 1)
     Array(int len = 1, int t = 1, int d = 10);
-
-    // Конструктор 2: по массиву
     Array(int *, int);
-
-    // Конструктор копирования
     Array(Array &);
-
-    // Деструктор
     ~Array();
-
-    // Оператор присваивания
     Array &operator=(Array &);
-
-    // Оператор индексации
     int &operator[](int);
-
-    // Проверка на упорядоченность по неубыванию
     bool Test();
-
-    // Оператор сравнения на равенство элементов массивов (но не порядка)
     bool operator==(Array);
-
-    // Сортировка Шелла
     void Shell_sort();
-
-    // Пирамидальная сортировка
     void Heapsort();
-
-    // Сортировка Хоара (Quick sort)
-    void Hoar_sort();
-
-    // Битоническая сортировка
+    void Hoar_sort(int, int);
     void Bit_sort();
-
-    // Перегрузка оператора ввода
     friend istream &operator>>(istream &, Array &);
-
-    // Перегрузка оператора вывода
     friend ostream &operator<<(ostream &, Array &);
 };
 
-Array::Array(int len, int t, int d) : n(len) {
+Array::Array(int len, int t, int d)
+{
+    n = len;
     a = new int[n];
-
-    if (t == 1) {
-        // Неупорядоченный массив
+    switch (t)
+    {
+    case 1:
         srand(time(NULL));
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             a[i] = rand() % d;
         }
-    } else if (t == 2) {
-        // Упорядоченный по неубыванию массив
-        for (int i = 0; i < n; ++i) {
-            a[i] = i + 1;
+        break;
+    case 2:
+        for (int i = n - 1; i >= 0; --i)
+        {
+            a[i] = i;
         }
-    } else if (t == 3) {
-        // Упорядоченный по невозрастанию массив
-        for (int i = 0; i < n; ++i) {
-            a[i] = n - i;
+        break;
+    case 3:
+        for (int i = 0; i < n; ++i)
+        {
+            a[i] = i;
         }
-    } else {
-        // Неправильное значение t
-        cout << "Invalid value of t" << endl;
+        break;
+    default:
+        cout << "INVALID t";
+        break;
     }
 }
 
-Array::Array(int *arr, int len) : n(len) {
+Array::Array(int *arr, int len)
+{
+    n = len;
     a = new int[n];
-    for (int i = 0; i < n; ++i) {
-        a[i] = arr[i];
-    }
+    memcpy(a, arr, n * sizeof(int));
 }
 
-Array::Array(Array &other) : n(other.n) {
+Array::Array(Array &other)
+{
+    n = other.n;
     a = new int[n];
-    for (int i = 0; i < n; ++i) {
-        a[i] = other.a[i];
-    }
+    memcpy(a, other.a, n * sizeof(int));
 }
 
-Array::~Array() {
+Array::~Array()
+{
     delete[] a;
+    a = NULL;
 }
 
-Array &Array::operator=(Array &other) {
-    if (this != &other) {
-        delete[] a;
+Array &Array::operator=(Array &other)
+{
+    if (this != &other)
+    {
         n = other.n;
+        delete[] a;
         a = new int[n];
-        for (int i = 0; i < n; ++i) {
-            a[i] = other.a[i];
-        }
+        memcpy(a, other.a, n * sizeof(int));
     }
     return *this;
 }
 
-int &Array::operator[](int index) {
-    if (index < 0 || index >= n) {
-        cout << "Out of range" << endl;
+int &Array::operator[](int i)
+{
+    if (i < 0 || i >= n)
+    {
+        cout << "OUT OF RANGE";
         return a[0];
     }
-    return a[index];
+    return a[i];
 }
 
-bool Array::Test() {
-    for (int i = 1; i < n; ++i) {
-        if (a[i - 1] > a[i]) {
-            return false;
+bool Array::Test()
+{
+    int test = 0;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (a[i] < a[i + 1])
+        {
+            ++test;
         }
     }
-    return true;
+    return test == (n - 1);
 }
 
-bool Array::operator==(Array other) {
-    if (n != other.n) {
-        return false;
+bool Array::operator==(Array other)
+{
+    return n == other.n && equal(a, a + n, other.a);
+}
+
+void Array::Shell_sort()
+{
+    const int maxSteps = static_cast<int>(log2(n)) + 1;
+    int arrOfSteps[maxSteps];
+    int step = n;
+    int stepCount = 0;
+    for (int i = 0; step != 1; ++i)
+    {
+        step /= 2;
+        arrOfSteps[stepCount++] = step;
     }
-    for (int i = 0; i < n; ++i) {
-        if (a[i] != other.a[i]) {
-            return false;
+    for (int s = 0; s < stepCount; ++s)
+    {
+        int gap = arrOfSteps[s];
+
+        for (int i = gap; i < n; ++i)
+        {
+            int temp = a[i];
+            int j;
+            for (j = i; j >= gap && a[j - gap] > temp; j -= gap)
+            {
+                a[j] = a[j - gap];
+            }
+            a[j] = temp;
         }
     }
-    return true;
 }
 
-void Array::Shell_sort() {
-    // Реализация сортировки Шелла
-    // TODO: Дополнить код сортировки Шелла
+void Array::Heapsort()
+{
+    for (int i = n / 2 - 1; i >= 0; --i)
+    {
+        int parent = i;
+        while (2 * parent + 1 < n)
+        {
+            int leftChild = 2 * parent + 1;
+            int rightChild = 2 * parent + 2;
+            int largest = parent;
+
+            if (leftChild < n && a[leftChild] > a[largest])
+            {
+                largest = leftChild;
+            }
+
+            if (rightChild < n && a[rightChild] > a[largest])
+            {
+                largest = rightChild;
+            }
+
+            if (largest != parent)
+            {
+                swap(a[parent], a[largest]);
+                parent = largest;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    for (int i = n - 1; i > 0; --i)
+    {
+        swap(a[0], a[i]);
+        int parent = 0;
+        while (2 * parent + 1 < i)
+        {
+            int leftChild = 2 * parent + 1;
+            int rightChild = 2 * parent + 2;
+            int largest = parent;
+
+            if (leftChild < i && a[leftChild] > a[largest])
+            {
+                largest = leftChild;
+            }
+
+            if (rightChild < i && a[rightChild] > a[largest])
+            {
+                largest = rightChild;
+            }
+
+            if (largest != parent)
+            {
+                swap(a[parent], a[largest]);
+                parent = largest;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
 }
 
-void Array::Heapsort() {
-    // Реализация пирамидальной сортировки
-    // TODO: Дополнить код пирамидальной сортировки
+void Array::Hoar_sort(int l, int r)
+{
+    if (r == -1)
+        r = n - 1;
+
+    if (l >= r)
+        return;
+
+    int x = a[(l + r) / 2];
+    int i = l - 1;
+    int j = r + 1;
+
+    while (true)
+    {
+        do
+        {
+            i++;
+        } while (a[i] < x);
+
+        do
+        {
+            j--;
+        } while (a[j] > x);
+
+        if (i >= j)
+            break;
+
+        swap(a[i], a[j]);
+    }
+
+    Hoar_sort(l, j);
+    Hoar_sort(j + 1, r);
 }
 
-void Array::Hoar_sort() {
-    // Реализация сортировки Хоара (Quick sort)
-    // TODO: Дополнить код сортировки Хоара
+void Array::Bit_sort()
+{
+    int maxBits = 32;
+    const int RADIX = 2;
+    int *output = new int[n];
+    int *count = new int[RADIX];
+    int shift = 0;
+
+    while (shift < maxBits)
+    {
+        memset(count, 0, RADIX * sizeof(int));
+
+        for (int i = 0; i < n; ++i)
+        {
+            int index = (a[i] >> shift) & 1;
+            count[index]++;
+        }
+
+        count[1] += count[0];
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            int index = (a[i] >> shift) & 1;
+            output[--count[index]] = a[i];
+        }
+
+        memcpy(a, output, n * sizeof(int));
+
+        shift++;
+    }
+
+    delete[] output;
+    delete[] count;
 }
 
-void Array::Bit_sort() {
-    // Реализация битонической сортировки
-    // TODO: Дополнить код битонической сортировки
-}
-
-istream &operator>>(istream &in, Array &arr) {
-    for (int i = 0; i < arr.n; ++i) {
-        in >> arr.a[i];
+istream &operator>>(istream &in, Array &array)
+{
+    cout << "Enter len";
+    in >> array.n;
+    cout << "Enter numbers";
+    for (int i = 0; i < array.n; ++i)
+    {
+        in >> array.a[i];
     }
     return in;
 }
 
-ostream &operator<<(ostream &out, Array &arr) {
-    for (int i = 0; i < arr.n; ++i) {
-        out << arr.a[i] << " ";
+ostream &operator<<(ostream &out, Array &array)
+{
+    for (int i = 0; i < array.n; ++i)
+    {
+        out << array.a[i] << " ";
     }
     out << endl;
     return out;
 }
 
-int main() {
-    // Пример использования класса Array
-
-    // Создание массива случайных чисел
-    Array arr1(10, 1, 100);
-    cout << "Original Array (Unordered): " << arr1;
-
-    // Создание упорядоченного массива по неубыванию
-    Array arr2(10, 2);
-    cout << "Original Array (Non-decreasing): " << arr2;
-
-    // Создание упорядоченного массива по невозрастанию
-    Array arr3(10, 3);
-    cout << "Original Array (Non-increasing): " << arr3;
-
-    // Создание массива по заданному массиву
-    int customArr[] = {5, 3, 8, 2, 1};
-    Array arr4(customArr, 5);
-    cout << "Original Array (Custom): " << arr4;
-
-    // Проверка метода Test
-    cout << "Is arr1 non-decreasing? " << (arr1.Test() ? "Yes" : "No") << endl;
-    cout << "Is arr2 non-decreasing? " << (arr2.Test() ? "Yes" : "No") << endl;
-
-    // Проверка оператора ==
-    cout << "Is arr2 equal to arr3? " << (arr2 == arr3 ? "Yes" : "No") << endl;
-    cout << "Is arr3 equal to arr4? " << (arr3 == arr4 ? "Yes" : "No") << endl;
-
-    // Пример сортировки Шелла
-    Array arr5(10, 1, 100);
-    cout << "Original Array (Unordered): " << arr5;
-    arr5.Shell_sort();
-    cout << "Sorted Array (Shell Sort): " << arr5;
-
-    // Пример пирамидальной сортировки
-    Array arr6(10, 1, 100);
-    cout << "Original Array (Unordered): " << arr6;
-    arr6.Heapsort();
-    cout << "Sorted Array (Heapsort): " << arr6;
-
-    // Пример сортировки Хоара (Quick sort)
-    Array arr7(10, 1, 100);
-    cout << "Original Array (Unordered): " << arr7;
-    arr7.Hoar_sort();
-    cout << "Sorted Array (Hoare Sort): " << arr7;
-
-    // Пример битонической сортировки
-    Array arr8(8, 1, 100);
-    cout << "Original Array (Unordered): " << arr8;
-    arr8.Bit_sort();
-    cout << "Sorted Array (Bitonic Sort): " << arr8;
-
+int main()
+{
+    Array a(10);
+    a.Heapsort();
+    cout << a;
     return 0;
 }
